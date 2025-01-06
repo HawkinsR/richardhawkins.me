dcf25a7e0c8f0fd3bff977c4f77daaf0ba787da4
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-01-06 14:56:02 -0600

    add angular routing, dockerfile, workflow

--- RHMeLanding/src/app/app.routes.ts
+++ RHMeLanding/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { CertificationsComponent } from './components/certifications/certifications.component'
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent,
+    title: 'Experience',
+  },
+  {
+    path: 'certifications',
+    component: CertificationsComponent,
+    title: 'Certifications',
+  },
+];
+
